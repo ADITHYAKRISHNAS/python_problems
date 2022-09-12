9a157248b280011e5262f44216db6d3478d813b6
Adithya Krishna S <adithyakrishnas2015@gmal.com>
2022-09-12 23:07:34 +0530

    sqr-rt

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
 # python program for hello world
-print("hello world")
+#print("hello world")
 # Add two number
-num1 = input('Enter the first number: ')
-num2 = input('Enter the second number: ')
-sum = float(num1) + float(num2)
-print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
+#num1 = input('Enter the first number: ')
+#num2 = input('Enter the second number: ')
+#sum = float(num1) + float(num2)
+#print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
+# find the square root
+number = float(input("Enter the number"))
+Sqr_rt = number ** 0.5
+print("the square root of %f is %f"%(number,Sqr_rt))
