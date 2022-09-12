cab800b0382e5c9496d3553695b588a75b6dd3ac
Adithya Krishna S <adithyakrishnas2015@gmal.com>
2022-09-12 22:05:16 +0530

    add two number added

--- python.py
+++ python.py
@@ -1,2 +1,7 @@
 # python program for hello world
 print("hello world")
+# Add two number
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
